6d5d7db00bf1b28cd308bac256e0c29512fbe0da
Anibal Olivares <olivaresluisanibal@gmail.com>
2022-10-16 10:42:48 -0400

    fix: window is not define

--- modules/shared/useLocalStorage.ts
+++ modules/shared/useLocalStorage.ts
@@ -3,10 +3,11 @@ import { useState } from 'react'
 export const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => void] => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      if (window) {
+      if (globalThis.window) {
         const item = window.localStorage.getItem(key)
         return item ? JSON.parse(item) : initialValue
       }
+      return initialValue
     } catch (error) {
       console.log(error)
       return initialValue
@@ -17,7 +18,7 @@ export const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T)
     try {
       const valueToStore = value instanceof Function ? value(storedValue) : value
       setStoredValue(valueToStore)
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      if (globalThis.window) window.localStorage.setItem(key, JSON.stringify(valueToStore))
     } catch (error) {
       console.log(error)
     }
